a1b9f90da9901045e52ed3b11a6cc3e640804ed6
Lindsey Davenport <ldavenport2@atu.edu>
2023-11-12 19:40:49 -0600

    Edits

--- Pages/Department.cshtml.cs
+++ Pages/Department.cshtml.cs
@@ -1,12 +1,20 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MyCoreApp.Pages
 {
     public class DepartmentModel : PageModel
     {
+        private readonly ILogger<DepartmentModel> _logger;
+
+        public DepartmentModel(ILogger<DepartmentModel> logger)
+        {
+            _logger = logger;
+        }
+
         public void OnGet()
         {
+            string dateTime = DateTime.Now.ToShortDateString();
+            ViewData["TimeStamp"] = dateTime;
         }
     }
 }
